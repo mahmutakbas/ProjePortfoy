6bc406fc38450edd6e66da23b7e31672474c5717
Mahmut <mahmuttakbass@gmail.com>
2023-05-10 23:10:11 +0300

    new

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 //DB
 builder.Services.AddTransient<IProjeDal, ProjeDal>();
